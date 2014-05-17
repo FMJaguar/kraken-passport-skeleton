b5ac76f57fc743ca090427d6ae2301d34208f874
Rich DeLauder <rich.delauder@gmail.com>
2014-05-17 07:54:41 -0700

    added db

--- index.js
+++ index.js
@@ -3,12 +3,15 @@
 
 var kraken = require('kraken-js'),
     app = require('express')(),
+    nconf = require('nconf'),
     options = require('./lib/spec')(app),
+    db = require ('./lib/database'),
     port = process.env.PORT || 8000;
 
+    nconf.argv().env().file('config/config.json');
+    db.config(nconf.get('databaseConfig'));
 
-app.use(kraken(options));
-
+    app.use(kraken(options));
 
 app.listen(port, function (err) {
     console.log('[%s] Listening on http://localhost:%d', app.settings.env, port);
