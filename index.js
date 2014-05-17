cab48f7b0545a267489541acf98a172d8fe77366
Rich DeLauder <rich.delauder@gmail.com>
2014-05-17 08:55:21 -0700

    auth added

--- index.js
+++ index.js
@@ -6,10 +6,26 @@ var kraken = require('kraken-js'),
     nconf = require('nconf'),
     options = require('./lib/spec')(app),
     db = require ('./lib/database'),
+    passport = require ('passport'),
+    auth = require('./lib/auth'),
+    //User = require('./models/User'),
     port = process.env.PORT || 8000;
 
     nconf.argv().env().file('config/config.json');
     db.config(nconf.get('databaseConfig'));
+
+    passport.use(auth.localStrategy());
+
+    //Give passport a way to serialize and deserialize a user. In this case, by the user's id.
+    passport.serializeUser(function (user, done) {
+        done(null, user.id);
+    });
+
+    passport.deserializeUser(function (id, done) {
+        User.findOne({_id: id}, function (err, user) {
+            done(null, user);
+        });
+    });
 
     app.use(kraken(options));
 
