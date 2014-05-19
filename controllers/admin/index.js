8c0687f188d0589b78c6a1d71e39a3bea56169ef
Rich DeLauder <rich.delauder@gmail.com>
2014-05-18 17:07:44 -0700

    must be admin to view admin route

--- controllers/admin/index.js
+++ controllers/admin/index.js
@@ -1,18 +1,16 @@
 'use strict';
 
 
-var AdminModel = require('../../models/admin');
+var AdminModel = require('../../models/admin'),
+	auth = require('../../lib/auth');
 
 
 module.exports = function (router) {
 
     var model = new AdminModel();
 
-
-    router.get('/', function (req, res) {
-        
+    router.get('/', auth.isAuthenticated('admin') ,function (req, res) {
         res.render('admin/index', model);
-        
     });
 
 };
